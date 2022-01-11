2f28e8088bdb43f3108889a3853f54d5a3006e88
WolfInaSuit <Stevferro1@gmail.com>
2022-01-10 23:44:11 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'    
+
+import Coins from './components/Coins'  
+import Navbar from './components/Navbar'                         
+
+function App() {
+
+  const [coins, setCoins] = useState([])
+
+
+  /* Crypto API URL */
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false'
+
+  /* Could change amount per @ "page=" */
+
+
+  /* How to Fetch data from API below */
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data)
+      console.log(response.data[0])
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
+  return (
+    <>
+    <Navbar  /> 
+    <Coins  coins={coins}/>
+    </>
+  );
+}
+
+export default App;
